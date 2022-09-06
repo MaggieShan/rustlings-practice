3589747e7ceb355d12f77c27206da99a56a8bef9
Maggie Shan <sshan.maggie@gmail.com>
2022-09-05 22:05:06 -0400

    lesson 2: fn

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,20 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// Lesson: 
+// * Rust code uses snake case
+// * functions can be defined before or after where they're used, 
+//   as long as they're defined somewhere in the scope that can be seen by the caller
+
+fn call_me2() {
+    println!("Hello to you 2!");
+}
 
 fn main() {
     call_me();
+    call_me2();
+}
+
+fn call_me() {
+    println!("Hello, World!");
 }
--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,7 +1,9 @@
 // functions5.rs
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// Lesson: without a semi colon, the expression becomes the return value of the function
+// * either add a return statement (usually for early returns)
+// * or remove the semi-colon and let the expression be the return value
 
 fn main() {
     let answer = square(3);
@@ -9,5 +11,5 @@ fn main() {
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
